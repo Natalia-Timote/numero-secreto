fbeed912e58be6b2246a8ce9b2838fd19c949a86
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-04-28 00:03:01 -0300

    adicionando javascript

---
+++ app/number-sort.js
@@ -0,0 +1,14 @@
+const minValue = 1;
+const maxValue = 100;
+
+const secretNumber = newSecretNumber();
+
+function newSecretNumber() {
+    return parseInt(Math.random * maxValue + 1);
+}
+
+const elementMinValue = document.getElementById(min-value);
+elementMinValue.innerHTML = minValue;
+
+const elementMaxValue = document.getElementById(max-value);
+elementMaxValue.innerHTML = maxValue;
