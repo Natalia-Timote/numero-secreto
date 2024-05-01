dd16444b7a38becb8f4c9824b862a859191651b4
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-04-30 22:02:13 -0300

    reiniciar e game over

--- app/validation.js
+++ app/validation.js
@@ -2,8 +2,23 @@ function guessValidValue(guess) {
     const number = +guess; //Tornando o número inteiro
 
     if(invalidGuess(number)) {
+        if(guess.toUpperCase() === 'GAME OVER') {
+            document.body.innerHTML = `
+            <h2>Game over!</h2>
+            <button class="play-again__btn" id="play-again"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+            `;
+    
+            const play = document.getElementById('play-again');
+    
+            play.addEventListener('click', () => {
+            window.location.reload();
+            })
+            
+            document.body.style.backgroundColor = "var(--secondary-color)";
+        } else {
         guessElement.innerHTML += '<div>Valor inválido</div>';
         return;
+        }
     }
 
     if(numberNotAllowed(number)) {
@@ -15,12 +30,21 @@ function guessValidValue(guess) {
         document.body.innerHTML = `
         <h2>Parabéns, você acertou!</h2>
         <h3>O Número Secreto era ${number}</h3>
+
+        <button class="play-again__btn" id="play-again"><i class="fa-solid fa-arrow-rotate-left"></i></button>
         `;
+
+        const play = document.getElementById('play-again');
+
+        play.addEventListener('click', () => {
+        window.location.reload();
+        })
     } else if(number > secretNumber) {
         guessElement.innerHTML += `<div>O Número Secreto é menor <i class="fa-solid fa-circle-arrow-down"></i></div>`
     }   else {
         guessElement.innerHTML += `<div>O Número Secreto é maior <i class="fa-solid fa-circle-arrow-up"></i></div>`
     }
+
 }
 
 function invalidGuess(number) {
